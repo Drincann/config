572bc988c42da038de7c9782eb766f773b4278b6
drincann <1019933576@qq.com>
2024-03-02 19:43:05 +0800

    modify(nvim): lua debugger

--- nvim/lua/debugger/conf.lua
+++ nvim/lua/debugger/conf.lua
@@ -51,5 +51,40 @@ return {
         justMyCode = false,
       },
     }
+  end,
+  lua = function()
+    local dap = require('dap')
+    dap.adapters.lua = {
+      type = "executable",
+      command = "node",
+      args = {
+        "/Users/drincanngao/lib/local-lua-debugger-vscode/extension/debugAdapter.js"
+      },
+      enrich_config = function(config, on_config)
+        if not config["extensionPath"] then
+          local c = vim.deepcopy(config)
+          -- 💀 If this is missing or wrong you'll see
+          -- "module 'lldebugger' not found" errors in the dap-repl when trying to launch a debug session
+          c.extensionPath = "/Users/drincanngao/lib/local-lua-debugger-vscode/"
+          on_config(c)
+        else
+          on_config(config)
+        end
+      end,
+    }
+
+    dap.configurations.lua = {
+      {
+        name = 'Current file (local-lua-dbg, lua)',
+        type = 'lua',
+        request = 'launch',
+        cwd = '${workspaceFolder}',
+        program = {
+          lua = 'lua',
+          file = '${file}',
+        },
+        args = {},
+      },
+    }
   end
 }
